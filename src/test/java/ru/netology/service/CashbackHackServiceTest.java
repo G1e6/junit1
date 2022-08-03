bd408493081a91d962723f240e9fc87725a06db4
G1e6 <gleb-potapov@rambler.ru>
2022-08-03 08:57:13 +0500

    test

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,32 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CashbackHackServiceTest {
+
+    @Test
+    void remain() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+            int amount = 1000;
+
+            int actual = cashbackHackService.remain(amount);
+            int expected = 0;
+
+         Assertions.assertEquals(expected, actual);
+        };
+
+    @Test
+    void remain1() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 700;
+
+        int actual = cashbackHackService.remain(amount);
+        int expected = 300;
+
+        Assertions.assertEquals(expected, actual);
+    };
+
+
+
+}
