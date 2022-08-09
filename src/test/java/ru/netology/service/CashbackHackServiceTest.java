771805529b5ae261c99b603e2a953456b7eab5dd
G1e6 <gleb-potapov@rambler.ru>
2022-08-09 17:35:44 +0500

    Assert

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -4,19 +4,21 @@ package ru.netology.service;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-
 public class CashbackHackServiceTest {
 
     @Test
+
     public void remain() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1000;
 
-        int expected = 0;
         int actual = cashbackHackService.remain(amount);
+        int expected = 0;
 
-        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
     }
+
+
 
     @Test
     public void remain1() {
@@ -26,8 +28,9 @@ public class CashbackHackServiceTest {
         int actual = cashbackHackService.remain(amount);
         int expected = 300;
 
-        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
     }
+
 
 
     @Test
@@ -38,21 +41,9 @@ public class CashbackHackServiceTest {
         int actual = cashbackHackService.remain(amount);
         int expected = 300;
 
-        Assert.assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
     }
 
-
-
-    @Test
-    public void remain3() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1700;
-
-        int actual = cashbackHackService.remain(amount);
-        int expected = 300;
-
-        Assert.assertEquals(expected, actual);
-    }
 
 
 
