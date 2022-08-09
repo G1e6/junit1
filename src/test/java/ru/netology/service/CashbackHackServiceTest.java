8e19b039117dc68c81d94c9505ee6b6510f7bc7f
G1e6 <gleb-potapov@rambler.ru>
2022-08-09 15:58:07 +0500

    testService

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -11,9 +11,9 @@ class CashbackHackServiceTest {
             int amount = 1000;
 
             int actual = cashbackHackService.remain(amount);
-            int expected = 0;
+            int expected = 1000;
 
-         Assertions.assertEquals(expected, actual);
+         Assertions.assertEquals(actual, expected);
         };
 
     @Test
@@ -24,9 +24,18 @@ class CashbackHackServiceTest {
         int actual = cashbackHackService.remain(amount);
         int expected = 300;
 
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertEquals(actual, expected);
     };
 
+    @Test
+    void remain2() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1700;
 
+        int actual = cashbackHackService.remain(amount);
+        int expected = 300;
+
+        Assertions.assertEquals(actual, expected);
+    };
 
 }
